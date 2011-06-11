97715b180603184a6646655fd91ee1bfda0a418a
高橋 啓治郎 <keijiro@Keijiros-MacBook-Air.local>
2011-06-11 15:59:07 +0900

    Implemented scene manager tools.

--- Assets/Editor/SceneSaveHook.js
+++ Assets/Editor/SceneSaveHook.js
@@ -2,9 +2,8 @@ class SceneSaveHook extends SaveAssetsProcessor {
 	static function OnWillSaveAssets(paths : String[]) : String[] {
         for (var path : String in paths) {
         	if (path.EndsWith(".unity")) {
-				for (var placeholder : GameObject in GameObject.FindGameObjectsWithTag("Placeholder")) {
-					GameObject.DestroyImmediate(placeholder);
-				}
+		       	var merger = GameObject.Find("Scene Merger").GetComponent.<SceneMerger>();
+        		merger.ClearOverlay();
         	}
         }
         return paths;
